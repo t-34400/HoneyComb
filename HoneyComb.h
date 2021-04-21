89092535acb146508901651bbd949c2150d56ae7
Takumi Miyoshi <34400mt@gmail.com>
2021-04-21 23:46:01 +0900

    プロジェクト ファイルを追加します。

---
+++ HoneyComb.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <array>
+#include <tuple>
+#include <vector>
+
+template <class T>
+class HoneyComb
+{
+public:
+	using honeyCombUnit_t = std::tuple<T, T>;
+
+private:
+	int m_maxRow{};
+	int m_maxCol{};
+
+	std::vector<honeyCombUnit_t> m_variables;
+
+	static constexpr NumberOfNearestNeighbor{ 3 };
+
+public:
+	HoneyComb();
+
+	double getCoordinateX(std::size_t index, bool isAbove) const;
+	double getCoordinateY(std::size_t index, bool isAbove) const;
+
+	std::array<T&, NumberOfNearestNeighbor> getNextNeighborSite();
+	std::array<const T&, NumberOfNearestNeighbor> getNextNeighborSite() const;
+};
